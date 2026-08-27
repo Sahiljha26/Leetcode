a3af983fdb27b108ad1f96fa08e11ecd879c3164
Sahiljha26 <Sahilbela4@gmail.com>
2026-08-27 07:14:29 +0530

    Time: 8 ms (7.43%) | Memory: 8.7 MB (99.28%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,20 @@
+bool isPalindrome(int x) {
+    int digit =0;
+    int long long  reverse =0;
+    int original = x;
+
+    if(x<0){
+        return false;
+    }
+
+    while(x!=0){
+        digit = x%10;
+        reverse = reverse * 10 + digit ;
+        x = x/10;
+    }
+
+    if( original == reverse ){
+        return true;
+    }
+     return false;
+}
